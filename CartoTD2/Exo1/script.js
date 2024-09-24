9e2c4afe23af92a2bf9c85eeb6667b564bc5d09f
Tom DC <tom.da-costa@etu.unice.fr>
2024-09-24 08:53:11 +0200

    up

--- CartoTD2/Exo1/script.js
+++ CartoTD2/Exo1/script.js
@@ -6,15 +6,14 @@ function onLoad() {
     }).addTo(map);
 
     L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+        .bindPopup('Votre Position').openPopup();
 
     map.locate({ setView: true, maxZoom: 16 });
     function onLocationFound(e) {
         var radius = e.accuracy / 2;
 
         L.marker(e.latlng).addTo(map)
-            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+            .bindPopup("Vous êtes à " + radius + " metre de ce point").openPopup();
 
         L.circle(e.latlng, radius).addTo(map);
         map.on('locationfound', onLocationFound);
