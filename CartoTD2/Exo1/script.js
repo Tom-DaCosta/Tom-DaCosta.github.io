831d608d83b7bbed1d6e64b4b4009cc0851d4001
Tom DC <tom.da-costa@etu.unice.fr>
2024-09-24 08:56:39 +0200

    up

--- CartoTD2/Exo1/script.js
+++ CartoTD2/Exo1/script.js
@@ -4,17 +4,28 @@ function onLoad() {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    
+
+    // Localiser l'utilisateur
     map.locate({ setView: true, maxZoom: 16 });
+
+    // Ajouter un marqueur à la position de l'utilisateur
     function onLocationFound(e) {
         var radius = e.accuracy / 2;
 
         L.marker(e.latlng).addTo(map)
-            .bindPopup("Vous êtes à " + radius + " metre de ce point").openPopup();
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
         L.circle(e.latlng, radius).addTo(map);
-        map.on('locationfound', onLocationFound);
     }
+
+    map.on('locationfound', onLocationFound);
+
+    // Gérer les erreurs de localisation
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
+    map.on('locationerror', onLocationError);
 }
 
 window.onload = onLoad;
