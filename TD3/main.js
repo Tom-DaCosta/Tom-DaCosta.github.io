91350f76b377efe4f66dcb3cad9a422e84fce3ec
Tom DA COSTA <tom.da-costa@etu.unice.fr>
2024-10-08 12:14:34 +0200

    re

--- TD3/main.js
+++ TD3/main.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
